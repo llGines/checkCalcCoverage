88a173ca9171d05764e3842d301c9a93212e5834
Lluis Gines <lluis.gines@ipronics.com>
2023-07-12 11:17:01 +0200

    calc done lint test  implemented

---
+++ calcClass.py
@@ -0,0 +1,34 @@
+import math
+
+class calc():
+
+    """this is just a mere class to try several aspects needed for ipronics job"""
+    
+    def __init__(self):
+        result=0
+    
+    def suma (self,a,b):
+        return a+b
+    def subs (a,b):
+        return a-b
+    def mult (a,b):
+        return a*b
+    def div (a,b):
+        return (a/b)
+    def power (a):
+        return pow(a,2)
+    def sqrot(a):
+        return  math.sqrt(a)
+
+        
+
+
+if __name__ == "__main__":
+
+    c=calc()
+
+    print(c.suma(3,2))
+
+        
+
+    
