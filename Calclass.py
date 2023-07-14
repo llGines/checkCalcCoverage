1d4f465c5e8ed91088fd6402ccc353ec6e363a94
Lluis Gines <lluis.gines@ipronics.com>
2023-07-14 12:16:41 +0200

    Added testing Calclass
    In order to create a robist calculator app, several test had been
    programmed

--- Calclass.py
+++ Calclass.py
@@ -1,36 +1,42 @@
 import math
 
-class Calclass():
+
+class Calclass:
 
     """this is just a mere class to try several aspects needed for ipronics job"""
-    
-    
-    def __init__(self):
-        result=0
-    
-    def suma (self,a,b):
-        return a+b
-    def subs (a,b):
-        return a-b
-    def mult (a,b):
-        return a*b
-    def div (a,b):
-        return (a/b)
-    def power (a):
-        return pow(a,2)
-    def sqrot(a):
-        return  math.sqrt(a)
+
+    # def __init__(self):
+    #     result=0
+
+    def suma(self, a, b):
+        return a + b
+
+    def subs(self, a, b):
+        return a - b
+
+    def mult(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
+    def power(self, a):
+        return pow(a, 2)
+
+    def sqrot(self, a):
+        return math.sqrt(a)
 
 
-        
+class TestClass:
+    calc = Calclass()
+
+    def test_sum(self):
+        ret = calc.suma(3, 2)
+        assert ret == 5
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    c=Calclass()
+#     c=Calclass()
 
-    print(c.suma(3,2))
-
-        
-
-    
+#     print(c.suma(3,2))
