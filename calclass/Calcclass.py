b239aa67d2c21a95d7ead2996a88ba658f0b6dbc
Lluis Gines <lluis.gines@ipronics.com>
2023-07-19 10:15:56 +0200

    stash after restructure

---
+++ calclass/Calcclass.py
@@ -0,0 +1,27 @@
+ 
+
+class Calclass:
+    # def __init__(self):
+
+
+    def suma(a, b):
+        return a + b
+
+    def subs(a, b):
+        return a - b
+
+    def mult(a, b):
+        return a * b
+
+    def div(a, b):
+        return a / b
+
+    def power(a):
+        if a>100:
+            print("false")
+        return pow(a, 2)
+
+    def sqrot(a):
+        return math.sqrt(a)
+
+    # print("done")
